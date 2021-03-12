9eb483fe6f4c290c3c36b15d9db98b160412e57a
Brent Tilley <brent.tilley@gmail.com>
2021-03-11 19:41:40 -0500

    Removed JSON from the PHP installation script.

--- install-php.sh
+++ install-php.sh
@@ -2,4 +2,4 @@
 
 add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get install php8.0 php8.0-cli php8.0-curl php8.0-ldap php8.0-json php8.0-mysql php8.0-zip php8.0-mbstring php8.0-xml php8.0-gd libapache2-mod-php8.0
+apt-get install php8.0 php8.0-cli php8.0-curl php8.0-ldap php8.0-mysql php8.0-zip php8.0-mbstring php8.0-xml php8.0-gd libapache2-mod-php8.0
