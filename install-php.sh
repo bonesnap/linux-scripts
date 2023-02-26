8a70bd88c1620cbc013a0c20a00830c6ec6cb62c
Brent Tilley <brent.tilley@gmail.com>
2023-02-26 22:24:45 +0000

    Updated PHP install script for 8.2.

--- install-php.sh
+++ install-php.sh
@@ -2,4 +2,4 @@
 
 add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get install php8.0 php8.0-cli php8.0-curl php8.0-ldap php8.0-mysql php8.0-zip php8.0-mbstring php8.0-xml php8.0-gd libapache2-mod-php8.0
+apt-get install php8.2 php8.2-cli php8.2-curl php8.2-ldap php8.2-mysql php8.2-zip php8.2-mbstring php8.2-xml php8.2-gd libapache2-mod-php8.2
