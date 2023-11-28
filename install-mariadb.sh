6c5d380e1c92f4e546379a36aa235c939db7b925
Brent Tilley <brent.tilley@gmail.com>
2023-11-28 17:37:18 -0500

    Updated MariaDB install script for version 10.11.

--- install-mariadb.sh
+++ install-mariadb.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-apt-get install apt-transport-https curl
-curl -o /etc/apt/trusted.gpg.d/mariadb_release_signing_key.asc 'https://mariadb.org/mariadb_release_signing_key.asc'
-sh -c "echo 'deb https://mirror.its.dal.ca/mariadb/repo/10.10/ubuntu bionic main' >>/etc/apt/sources.list"
+add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.11/ubuntu jammy main'
 
 apt-get update
 
 apt-get install mariadb-server && /usr/bin/mysql_secure_installation
+
+# More info:
+# https://mariadb.com/kb/en/installing-mariadb-deb-files/
