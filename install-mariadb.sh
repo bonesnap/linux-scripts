8915096f00cc57e4fab2ce24e4a55a120a59f158
Brent Tilley <brent.tilley@gmail.com>
2024-12-30 23:29:01 -0500

    Updated MariaDB install script for version 11.4.

--- install-mariadb.sh
+++ install-mariadb.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.11/ubuntu jammy main'
+apt-get update
+
+apt-get install apt-transport-https curl
+
+mkdir -p /etc/apt/keyrings
+
+curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
+
+touch /etc/apt/sources.list.d/mariadb.list
+
+echo 'deb [signed-by=/etc/apt/keyrings/mariadb-keyring.pgp] https://mirror.its.dal.ca/mariadb/repo/11.4/ubuntu noble main' > /etc/apt/sources.list.d/mariadb.list
 
 apt-get update
 
 apt-get install mariadb-server && /usr/bin/mysql_secure_installation
 
 # More info:
-# https://mariadb.com/kb/en/installing-mariadb-deb-files/
+# https://mariadb.org/download
