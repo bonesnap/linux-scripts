9e3e15850af1ecf5a44fba47768106433713d3de
Brent Tilley <brent.tilley@gmail.com>
2025-12-24 13:55:51 -0500

    Updated NodeJS install script for version 24.x.

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
 
 apt-get update
 apt-get install nodejs
 
-npm install -g grunt-cli
+#npm install -g grunt-cli
